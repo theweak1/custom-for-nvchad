fd827f08513fa7b3fe902cdb50e69edbcfa3a07c
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2023-08-11 11:01:24 -0400

    feat(plugins): added NvChad/ui to custom plugins

--- plugins.lua
+++ plugins.lua
@@ -10,6 +10,18 @@ local plugins = {
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
     end,
+  },
+  {
+    "NvChad/ui",
+    branch = "v2.0",
+    lazy = false,
+    config = function()
+      require "nvchad"
+      vim.api.nvim_command('highlight Normal ctermbg=236')
+      vim.api.nvim_command('highlight Normal ctermfg=NONE')
+      vim.api.nvim_command('highlight Normal cterm=NONE')
+      vim.api.nvim_command('highlight Normal guibg=NONE')
+    end,
   }
 }
 return plugins

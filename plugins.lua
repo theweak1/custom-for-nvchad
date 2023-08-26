ebc96fa3cfab1fb2e25791775005f6c3e8e9530e
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2023-08-26 14:33:24 -0400

    Update plugins.lua

--- plugins.lua
+++ plugins.lua
@@ -45,9 +45,56 @@ local plugins = {
 
        -- low level
         "c",
-        "zig"
       },
     },
+  },
+  {
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+    opts = function()
+      return {
+        ensure_installed = {
+
+          -- Web development
+          "css-lsp",
+          "deno",
+          "grammarly-languageserver",
+          "html-lsp",
+          "json-lsp",
+          "jsonlint",
+          "prisma-language-server",
+          "tailwindcss-language-server",
+          "typescript-language-server",
+
+          -- Core
+          "lua-language-server",
+          
+          -- Markdown
+          "markdown-toc",
+          "markdownlint",
+          
+          -- formatter
+          "prettier",
+          "prettierd",
+         
+          -- Yaml for ansible
+          -- "yaml-language-server",
+          -- "yamllint",
+          -- "yamlfix",
+          -- "yamlfmt",
+        },
+      }
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "mason")
+      require("mason").setup(opts)
+
+      -- Custom nvchad cmd to install all mason binaries listed
+      vim.api.nvim_create_user_command("MasonInstallAll", function()
+        vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+      end, {})
+      vim.g.mason_binaries_list = opts.ensure_installed
+    end,
   },
 }
 return plugins
